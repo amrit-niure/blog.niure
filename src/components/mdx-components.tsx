bffb18541897a552ae65741fdca1e0074549e7fa
Amrit Niure <niureamrit2@gmail.com>
2023-11-13 13:02:38 +1100

    type validation

--- src/components/mdx-components.tsx
+++ src/components/mdx-components.tsx
@@ -6,7 +6,7 @@ import { cn } from "@/lib/utils"
 
 
 const components = {
-  h1: ({ className, ...props }) => (
+  h1: ({ className, ...props }:{ className?: string; [key: string]: any }) => (
     <h1
       className={cn(
         "mt-2 scroll-m-20 text-4xl font-bold tracking-tight",
@@ -15,7 +15,7 @@ const components = {
       {...props}
     />
   ),
-  h2: ({ className, ...props }) => (
+  h2: ({ className, ...props }:{ className?: string; [key: string]: any }) => (
     <h2
       className={cn(
         "mt-10 scroll-m-20 border-b pb-1 text-3xl font-semibold tracking-tight first:mt-0",
@@ -24,7 +24,7 @@ const components = {
       {...props}
     />
   ),
-  h3: ({ className, ...props }) => (
+  h3: ({ className, ...props }:{ className?: string; [key: string]: any }) => (
     <h3
       className={cn(
         "mt-8 scroll-m-20 text-2xl font-semibold tracking-tight",
@@ -33,7 +33,7 @@ const components = {
       {...props}
     />
   ),
-  h4: ({ className, ...props }) => (
+  h4: ({ className, ...props }:{ className?: string; [key: string]: any }) => (
     <h4
       className={cn(
         "mt-8 scroll-m-20 text-xl font-semibold tracking-tight",
@@ -42,7 +42,7 @@ const components = {
       {...props}
     />
   ),
-  h5: ({ className, ...props }) => (
+  h5: ({ className, ...props }:{ className?: string; [key: string]: any }) => (
     <h5
       className={cn(
         "mt-8 scroll-m-20 text-lg font-semibold tracking-tight",
@@ -51,7 +51,7 @@ const components = {
       {...props}
     />
   ),
-  h6: ({ className, ...props }) => (
+  h6: ({ className, ...props }:{ className?: string; [key: string]: any }) => (
     <h6
       className={cn(
         "mt-8 scroll-m-20 text-base font-semibold tracking-tight",
@@ -60,28 +60,28 @@ const components = {
       {...props}
     />
   ),
-  a: ({ className, ...props }) => (
+  a: ({ className, ...props }:{ className?: string; [key: string]: any }) => (
     <a
       className={cn("font-medium underline underline-offset-4", className)}
       {...props}
     />
   ),
-  p: ({ className, ...props }) => (
+  p: ({ className, ...props }:{ className?: string; [key: string]: any }) => (
     <p
       className={cn("leading-7 [&:not(:first-child)]:mt-6", className)}
       {...props}
     />
   ),
-  ul: ({ className, ...props }) => (
+  ul: ({ className, ...props }:{ className?: string; [key: string]: any }) => (
     <ul className={cn("my-6 ml-6 list-disc", className)} {...props} />
   ),
-  ol: ({ className, ...props }) => (
+  ol: ({ className, ...props }:{ className?: string; [key: string]: any }) => (
     <ol className={cn("my-6 ml-6 list-decimal", className)} {...props} />
   ),
-  li: ({ className, ...props }) => (
+  li: ({ className, ...props }:{ className?: string; [key: string]: any }) => (
     <li className={cn("mt-2", className)} {...props} />
   ),
-  blockquote: ({ className, ...props }) => (
+  blockquote: ({ className, ...props }:{ className?: string; [key: string]: any }) => (
     <blockquote
       className={cn(
         "mt-6 border-l-2 pl-6 italic [&>*]:text-muted-foreground",
@@ -110,7 +110,7 @@ const components = {
       {...props}
     />
   ),
-  th: ({ className, ...props }) => (
+  th: ({ className, ...props }:{ className?: string; [key: string]: any }) => (
     <th
       className={cn(
         "border px-4 py-2 text-left font-bold [&[align=center]]:text-center [&[align=right]]:text-right",
@@ -119,7 +119,7 @@ const components = {
       {...props}
     />
   ),
-  td: ({ className, ...props }) => (
+  td: ({ className, ...props }:{ className?: string; [key: string]: any }) => (
     <td
       className={cn(
         "border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right",
@@ -128,7 +128,7 @@ const components = {
       {...props}
     />
   ),
-  pre: ({ className, ...props }) => (
+  pre: ({ className, ...props }:{ className?: string; [key: string]: any }) => (
     <pre
       className={cn(
         "mb-4 mt-6 overflow-x-auto rounded-lg border bg-black py-4",
@@ -137,7 +137,7 @@ const components = {
       {...props}
     />
   ),
-  code: ({ className, ...props }) => (
+  code: ({ className, ...props }:{ className?: string; [key: string]: any }) => (
     <code
       className={cn(
         "relative rounded border px-[0.3rem] py-[0.2rem] font-mono text-sm",
